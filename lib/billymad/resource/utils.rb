c96fc241055653439d01002ada699736cc08b4bc
Andrejs Eisaks <eisaks.andrejs@gmail.com>
2023-04-04 00:48:18 +0200

    Add #underscore to fix dashed API attributes

--- lib/billymad/resource/utils.rb
+++ lib/billymad/resource/utils.rb
@@ -1,16 +1,29 @@
 module Billymad
   module Resource
     module Utils
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
 
-      def demodulize(path)
-        path = path.to_s
-        if i = path.rindex('::')
-          path[(i+2)..-1]
-        else
-          path
+      module ClassMethods
+        def demodulize(path)
+          path = path.to_s
+          if i = path.rindex('::')
+            path[(i + 2)..-1]
+          else
+            path
+          end
         end
       end
 
+      def underscore(camel_cased_word)
+        camel_cased_word.to_s.gsub(/::/, '/')
+                        .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+                        .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+                        .tr('-', '_')
+                        .downcase
+      end
     end
   end
 end
+
--- spec/billymad/resource/utils_spec.rb
+++ spec/billymad/resource/utils_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
 describe Billymad::Resource::Utils do
-  let(:described_class) { Class.new { extend Billymad::Resource::Utils } }
-
   describe '#demodulize' do
+    let(:described_class) { Class.new { include Billymad::Resource::Utils } }
+
     it 'extract correct class name from namespaced class' do
       expect(described_class.demodulize('My::ProperClass')).to eq('ProperClass')
     end
   end
-end
 
+  describe '#underscore' do
+    let(:described_class) { Class.new { extend Billymad::Resource::Utils } }
+
+    it do
+      expect(described_class.underscore('text')).to eq('text')
+      expect(described_class.underscore('camelCase')).to eq('camel_case')
+      expect(described_class.underscore('dash-case')).to eq('dash_case')
+    end
+  end
+end
