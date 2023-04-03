20757a1eeeb5ecf3a8721b7f3fa5bf13746f01c0
Andrejs Eisaks <eisaks.andrejs@gmail.com>
2023-04-04 00:55:46 +0200

    Fix keyword arguments (Ruby 3)

--- lib/billymad/request/proxy.rb
+++ lib/billymad/request/proxy.rb
@@ -2,19 +2,18 @@ module Billymad
   module Request
     class Proxy
       class << self
-
         def get(resource, options = {})
-          info = Billymad::Request::Info.new(resource, options.merge(method: :get))
+          info = Billymad::Request::Info.new(resource, **options.merge(method: :get))
           Billymad::Request::Base.new(info).perform
         end
 
         def post(resource, options = {})
-          info = Billymad::Request::Info.new(resource, options.merge(method: :post))
+          info = Billymad::Request::Info.new(resource, **options.merge(method: :post))
           Billymad::Request::Base.new(info).perform
         end
 
         def put(resource, options = {})
-          info = Billymad::Request::Info.new(resource, options.merge(method: :put))
+          info = Billymad::Request::Info.new(resource, **options.merge(method: :put))
           Billymad::Request::Base.new(info).perform
         end
 
@@ -22,8 +21,8 @@ module Billymad
           info = Billymad::Request::Info.new(resource, id: record_id, method: :delete)
           Billymad::Request::Base.new(info).perform
         end
-
       end
     end
   end
 end
+
